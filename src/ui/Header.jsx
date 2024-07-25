c132ac9b31ab11bf0036743fedc34e7307b40961
Igor Glushko <superigorek74@gmail.com>
2024-07-26 02:06:30 +0500

    Header template

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,8 +1,27 @@
+import { Link } from "react-router-dom";
+
 function Header() {
     return (
-        <header>
-            <h1>Django Store</h1>
-            <input type="text" />        
+        <header className="container-fluid" style={{marginTop: '20px'}}>
+            <div className="row" style={{justifyContent: "space-evenly"}}>
+                <div className="col-1">
+                    <h4>
+                        <Link className="navbar-brand" to={"/"}>Django Store</Link>
+                    </h4>
+                </div>
+                <div className="col-2" style={{alignContent: 'center'}}>
+                    <button className="btn btn-primary btn-lg" style={{borderRadius: '10px'}}>Seacrh by categories</button>
+                </div>
+                <div className="col-5">
+                    <form className="d-flex" style={{"background": "blue"}} role="search" >
+                        <input className="form-control me-2" style={{border: "2px solid blue"}} type="search" placeholder="Search products" />
+                        <input style={{borderRadius: '10px'}} className="btn btn-lg btn-primary" type="submit" placeholder="Find" />
+                    </form>
+                </div>
+                <div className="col-1">
+                    <button style={{border: "0px", background: 'white'}}>Geolocation</button>
+                </div>
+            </div>
         </header>
     );
 }
