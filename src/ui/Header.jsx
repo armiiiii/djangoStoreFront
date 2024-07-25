2d792da55ea93a2d1bc3f8a2b258c27830131ff6
Igor Glushko <superigorek74@gmail.com>
2024-07-26 02:25:56 +0500

    Update header template

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,19 +3,17 @@ import { Link } from "react-router-dom";
 function Header() {
     return (
         <header className="container-fluid" style={{marginTop: '20px'}}>
-            <div className="row" style={{justifyContent: "space-evenly"}}>
+            <div className="row" style={{justifyContent: 'space-evenly'}}>
                 <div className="col-1">
                     <h4>
                         <Link className="navbar-brand" to={"/"}>Django Store</Link>
                     </h4>
                 </div>
-                <div className="col-2" style={{alignContent: 'center'}}>
-                    <button className="btn btn-primary btn-lg" style={{borderRadius: '10px'}}>Seacrh by categories</button>
-                </div>
-                <div className="col-5">
-                    <form className="d-flex" style={{"background": "blue"}} role="search" >
-                        <input className="form-control me-2" style={{border: "2px solid blue"}} type="search" placeholder="Search products" />
-                        <input style={{borderRadius: '10px'}} className="btn btn-lg btn-primary" type="submit" placeholder="Find" />
+                <div className="col-6 d-flex" style={{alignContent: 'center'}}>
+                    <button className="btn btn-primary " style={{borderRadius: '10px'}}>Seacrh by categories</button>
+                    <form className="d-flex" style={{"background": "blue", width: '570px', borderRadius: '10px'}} role="search" >
+                        <input className="form-control me-1" style={{border: "2px solid blue"}} type="search" placeholder="Search products" />
+                        <input style={{borderRadius: '10px'}} className="btn btn-primary" type="submit" placeholder="Find" />
                     </form>
                 </div>
                 <div className="col-1">
