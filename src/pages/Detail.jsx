f8897fb2b5df87ebec4d3bc81198e7181af79659
Igor Glushko <superigorek74@gmail.com>
2024-08-27 23:14:46 +0500

    Detail page added

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,5 +1,23 @@
-function Detail() {
-    return (<></>);
+import { useEffect, useState } from "react";
+import { getProductsDetail } from "../api";
+
+
+
+function Detail({ id }) {
+    const [detail, setDetail] = useState({});
+
+    useEffect(() => {
+        const getDetail = () => {
+            const data = getProductsDetail(id);
+            setDetail(data)
+        }
+    }, [])
+    
+    return (
+        <main>
+            
+        </main>
+    )
 }
 
 export default Detail;
